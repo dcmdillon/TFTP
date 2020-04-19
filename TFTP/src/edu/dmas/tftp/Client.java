fed53452a7ec6cedfca94a0403efc7bc1f92f50c
dcmdillon <60584135+dcmdillon@users.noreply.github.com>
2020-04-19 11:35:23 -0500

    client changes

--- TFTP/src/edu/dmas/tftp/Client.java
+++ TFTP/src/edu/dmas/tftp/Client.java
@@ -1,5 +1,10 @@
 package edu.dmas.tftp;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Client {
 	
 	
@@ -10,9 +15,41 @@ public class Client {
 		//initializer
 	}
 	
-	public void requestFile() {
+	public void requestFile(String add, String rType, String Mode,String fileName) {
 		//this will be called from TFTP.java as the 'run' method
 		//setup options and special varibles beforehand, then call this method
+		try {
+			DatagramSocket socket = new DatagramSocket();
+			
+			
+			byte[] buf;
+			
+			if(rType.equalsIgnoreCase("Get")) {
+				buf = new byte[]{};
+			}
+			else {
+				
+			}
+			
+			
+			
+			InetAddress address = InetAddress.getByName(add);
+			
+			
+			
+			
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
 	}
 
 }
