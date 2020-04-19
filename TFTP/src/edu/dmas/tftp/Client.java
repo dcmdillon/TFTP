b04aa9e38858a02f1d3d86bd3286eca83b154a91
dcmdillon <60584135+dcmdillon@users.noreply.github.com>
2020-04-19 14:09:47 -0500

    additions to client, feel free to edit

--- TFTP/src/edu/dmas/tftp/Client.java
+++ TFTP/src/edu/dmas/tftp/Client.java
@@ -1,9 +1,11 @@
 package edu.dmas.tftp;
 
+import java.awt.Frame;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 public class Client {
 	
@@ -15,25 +17,48 @@ public class Client {
 		//initializer
 	}
 	
-	public void requestFile(String add, String rType, String Mode,String fileName) {
+	public void requestFile(String addr, String rType, String transferMode,String fileName) {
 		//this will be called from TFTP.java as the 'run' method
 		//setup options and special varibles beforehand, then call this method
 		try {
 			DatagramSocket socket = new DatagramSocket();
 			
 			
-			byte[] buf;
+			byte[] opcode;
+			byte[] fname = fileName.getBytes();
+			byte[] tmode = transferMode.getBytes();
+			
 			
 			if(rType.equalsIgnoreCase("Get")) {
-				buf = new byte[]{};
+				opcode = new byte[] {0,1};
 			}
 			else {
+				opcode = new byte[]{0,2};
+			}
+			
+			
+			byte[] buf = new byte[4 + fname.length + tmode.length];
+			
+			for(int i =0; i < buf.length; i++) {
+				
+				if(i < 2) {
+					
+				}
+				else if(i > 1 && i <= fname.length + 1) {
+					
+				}
+				else if(i == fname.length + 2) {
+					
+					
+					
+					
+				}
+				
 				
 			}
 			
 			
-			
-			InetAddress address = InetAddress.getByName(add);
+			InetAddress address = InetAddress.getByName(addr);
 			
 			
 			
